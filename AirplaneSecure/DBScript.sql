2a3c1c0de9b903e66ee82cfd2b8ef3c5d1478c96
theoclerc <115167981+theoclerc@users.noreply.github.com>
2022-10-20 19:30:10 +0200

    Update DBScript.sql

--- AirplaneSecure/DBScript.sql
+++ AirplaneSecure/DBScript.sql
@@ -17,9 +17,9 @@ CREATE TABLE [Ticket] (
 );
 
 INSERT User(Lastname,Firstname,Login,Password, Salt)
-	VALUES('Vialard','Celine','celvia','a1a6deda0b8d7ed3a72c07305987071ab205e964eddd8271a24411c9c98e0b04','bcgw!78'),
-	('Betrisey','Julienne','julbet','1019fc40a7983ecf8a1687697a37f3fedcd5deb7ad1492cf02285d5d3ab7762c','jfdL$3nskj'),
-	('Clerc','Theo','thecle','44cdf4a0998a7fb6e898b05d8377fc93baeeea0db83852d2cc04134f990ffa4c','Jbus453-');
+	VALUES('Vialard','Celine','celvia','oabe2guNftOnLAcwWYcHGrIF6WTt3YJxokQRycmOCwQ=','bcgw!78'),
+	('Betrisey','Julienne','julbet','EBn8QKeYPs+KFodpejfz/tzV3retFJLPAihdXTq3diw=','jfdL$3nskj'),
+	('Clerc','Theo','thecle','RM30oJmKf7bomLBdg3f8k7ru6g24OFLSzAQTT5kP+kw=','Jbus453-');
 
 INSERT Ticket(Name,IdUser)
 	VALUE('ticket1',1);
