d6f2c11d117853b358ce2db0c7a167213e9b87e6
celine.vialard2 <celine.vialard@students.hevs.ch>
2022-10-20 14:33:14 +0200

    Injection dependance

--- AirplaneSecure/Program.cs
+++ AirplaneSecure/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.AddScoped<ITicketDb, TicketDb>();
+builder.Services.AddScoped<IUserDb, UserDb>();
 
 builder.Services.AddSession(options =>
 {
